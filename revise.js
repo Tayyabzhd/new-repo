1c4f05836b7dbcb748901e5ee33574677e3ad2e5
asp <asp@Aptechnk1.edu>
2024-05-09 09:45:55 +0500

    message

---
+++ revise.js
@@ -0,0 +1,103 @@
+// let days = 7;
+// switch (days) {
+//     case 1:
+//         console.log("Monday");
+//         breake;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("Invalid Input");
+//         break;
+// }
+
+// let name = 'app';
+// if(name[0] === 'a' &&  name.length > 3){
+//     console.log('Good string')
+// } 
+// else{
+//     console.log('Bad string')
+// }
+
+// let msg = 'Invalid Input'
+// console.log(msg.slice(-5));
+
+// let msg='help'
+// console.log(msg.trim().toUpperCase())
+
+
+
+// let msg = 'apnaCollage'
+// console.log(msg.slice(4,9))
+// console.log(msg.indexOf('na'))
+// console.log(msg.replace('apna', 'our'))
+
+// console.log(msg.replace('l','t').replace('l','t'))
+
+// let followers = [ "khan" , "bilal" , "ayesha" , "marrukh" , "kinza" ];
+// followers.pop()
+// console.log (followers)
+// followers.shift()
+// console.log (followers)
+
+// let months= ["jan" , "jul" , "mar" ,"aug"]
+
+// months.shift("jan")
+// months.shift("jul")
+// months.unshift("jun")
+// months.unshift("jul")
+
+
+
+// console.log(months)
+
+
+
+// let arr = [['X','','O'],['','X',''],['O','','X']]
+// console.log(arr);
+
+// const FavMovie = 'Spiderman';
+// let guess = prompt('enter a fav movie name')
+// while (guess != FavMovie) {
+//     guess = prompt('Wrong try again')
+
+// }
+// if (guess == FavMovie) {
+//     console.log('True');
+// }
+
+
+// let fruits = ['mango', 'apple', 'pineapple', 'banana']
+
+
+// fruits.push('kiwi')
+
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(i + 1, fruits[i]);
+// }
+
+
+// let movies = [['spiderman', 'ironman', 'doc strange'],['superman', 'batman', 'flash']];
+
+// for (let i = 0; i < movies.length; i++) {
+//     console.log(` List ${i+1}`);
+//     for (let j = 0; j < movies[i].length; j++) {
+//         console.log(movies[i][j]);
+//     }
+// }
+
